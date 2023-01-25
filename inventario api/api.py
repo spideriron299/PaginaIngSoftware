de2a28c8ed29af19ba83278484b691e5b5d9d7f6
Fernando Labastida <labastidafernando460@gmail.com>
2023-01-24 21:27:21 -0600

    Ya quedó

--- inventario api/api.py
+++ inventario api/api.py
@@ -5,16 +5,11 @@ from flask_cors import CORS
 from flask import jsonify
 from db import obtener_conexion
 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
 app = Flask(__name__)
 CORS(app)
 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
+
 def hello_world():
     conexion = obtener_conexion()
 
@@ -25,9 +20,7 @@ def hello_world():
     conexion.close()
     return jsonify(productos)
 
-# main driver function
+
 if __name__ == '__main__':
 
-    # run() method of Flask class runs the application
-    # on the local development server.
     app.run()
